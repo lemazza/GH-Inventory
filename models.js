5c3d558ee228a4ad440c541ed0a56d1433ea4293
Louis Mazza <louismazza@gmail.com>
2019-03-21 12:27:34 -0700

    added request, setup model

--- models.js
+++ models.js
@@ -3,37 +3,46 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
-const ObjectId = mongoose.Schema.Types.ObjectId;
 
 /*
- * Games
+ * GameInventory
  */
 
-const GameListSchema = mongoose.Schema({
+//setting year as Number for simplicity
+const GameInventorySchema = mongoose.Schema({
+  bggId: Number,
   name: {type: String, required: true},
-  totalPlayCount: {type: Number, default: 0},
-  totalTableCount: {type: Number, default: 0},
-  playTimes: [Date],
-  tableTimes: [Date]
+  totalViewCount: {type: Number, default: 0},
+  thumbnail: String,
+  description: String,
+  year: Number,
+  minPlayers: Number,
+  maxPlayers: Number,
+  playTime: Number,
+  minAge: Number,
+  designer: [String],
+  location: String,
 })
 
-GameListSchema.virtual('totalCount').get( function() {
-  return (this.totalPlayCount || 0) + (this.totalTableCount || 0);
-})
 
-
-GameListSchema.methods.serialize = function() {
+GameInventorySchema.methods.serialize = function() {
   return {
     id: this._id,
     name: this.name,
-    totalCount: this.totalCount,
-    playCount: this.totalPlayCount,
-    tableCount: this.totalTableCount
+    thumbnail: this.thumbnail,
+    description: this.description,
+    year: this.year,
+    minPlayers: this.minPlayers,
+    maxPlayers: this.maxPlayers,
+    playTime: this.playTime,
+    minAge: this.minAge,
+    designer: this.designer,
+    location: this.location
   };
 };
 
 
 
-const Game = mongoose.model('Game', GameListSchema);
+const Game = mongoose.model('Game', GameInventorySchema);
 
 module.exports = {Game};
--- server.js
+++ server.js
@@ -1,115 +1,97 @@
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const {Game} = require('./models');
+const config = require('./config');
+const request = require('request')
+
+
+const { DATABASE_URL, PORT } = require('./config');
+
+const app = express();
+var http = require('http').Server(app);
+
+app.use(morgan('common'));
+app.use(bodyParser.json());
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Authorization, Content-Type, Accept");
+  res.header("Access-Control-Allow-Credentials", true);
+  next();
+});
+
+
+
+
+app.get('/', function (req, res, next) {
+  const list = ['Hi', 'Hello', 'Bonjour', 'Hey', 'Buenas Dias']
+
+  res.send(list);
+});
+
+
+
+
+app.get('/update/', function (req, res, next) {
+  const list = ['Hi', 'Hello', 'Bonjour', 'Hey', 'Buenas Dias']
+
+  console.log('about to request')
+
+  request('https://api.geekdo.com/xmlapi2/collection?username=gamehauscafe&own=1', function (error, response, body) {
+    console.log('requesting')
+    console.log('error:', error); // Print the error if one occurred
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    console.log('body:', body); // Print the HTML for the Google homepage.
+
+    res.send(body);
+  });
+
+});
 
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+function runServer(databaseUrl, port = PORT) {
+  return new Promise((resolve, reject) => {
+    mongoose.connect(databaseUrl, err => {
+      if (err) {
+        return reject(err);
+      }
+      server = http.listen(port, () => {
+        console.log(`Your app is listening on port ${port}`);
+        resolve();
+      })
+        .on('error', err => {
           mongoose.disconnect();
           reject(err);
-          return reject(err);
-        $inc: {totalPlayCount: 1},
-        $inc: {totalTableCount: 1},
-        $push: {playTimes: date}
-        $push: {tableTimes: date}
-        .on('error', err => {
-        console.log(`Your app is listening on port ${port}`);
-        if (err) {
-        resolve();
-        resolve();
-        return b.playCount - a.playCount
-        return b.totalCount - a.totalCount
-        return reject(err);
-        }
         });
-      console.log('Closing server');
-      if (a.totalCount === b.totalCount) {
-      if (err) {
-      server = http.listen(port, () => {
-      server.close(err => {
-      {
-      {
-      {"name": gameName},
-      {"name": gameName},
-      {upsert: true}
-      {upsert: true}
-      }
-      }
-      } else {
-      })
-      });
-      },
-      },
-    ).then(gameRet => console.log(gameRet))
-    ).then(gameRet => console.log(gameRet))
-    .findOneAndUpdate(
-    .findOneAndUpdate(
-    //sort games by totalCount followed by playCount
-    console.log(gameName, 'is a table game')
-    const serializedGames = allGames.map(game => game.serialize());
-    Game
-    Game
-    mongoose.connect(databaseUrl,{ useNewUrlParser: true }, err => {
-    res.status(200).json(serializedGames);
-    res.status(400).send(err);
-    return new Promise((resolve, reject) => {
-    serializedGames.sort((a, b) => {
-    })
     });
-    });
-  * RUNSERVER
-  */
-  .catch(err => {
-  .find()
-  .then(allGames => {
-  Game
-  let {date, playedGames, tableGames} = req.body;
-  playedGames.forEach(gameName=>{
-  res.send('Hellloooo from Board Game List thing');
-  res.status(201).send("posted")
-  return mongoose.disconnect().then(() => {
-  return new Promise((resolve, reject) => {
-  runServer(DATABASE_URL).catch(err => console.error(err));
-  tableGames.forEach(gameName=>{
-  })
-  })
-  })
-  })
   });
-  });
- /*
-app.get('/', function (req, res, next) {
-app.get('/games', function (req, res, next) {
-app.post('/games', function (req, res, next) {
-app.use(bodyParser.json());
-app.use(morgan('common'));
-const app = express();
-const bodyParser = require('body-parser');
-const express = require('express');
-const mongoose = require('mongoose');
-const morgan = require('morgan');
-const { DATABASE_URL, PORT } = require('./config');
-const {Game} = require('./models');
+}
+
+
 function closeServer() {
-function runServer(databaseUrl, port = PORT) {
+  return mongoose.disconnect().then(() => {
+    return new Promise((resolve, reject) => {
+      console.log('Closing server');
+      server.close(err => {
+        if (err) {
+          return reject(err);
+        }
+        resolve();
+      });
+    });
+  });
+}
+
+
 if (require.main === module) {
+  runServer(DATABASE_URL).catch(err => console.error(err));
+}
+
+
 module.exports = { runServer, app, closeServer };
-var http = require('http').Server(app);
-}
-}
-}
-})
-})
-});
